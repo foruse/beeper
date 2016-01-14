cfc13b21635885645a4d405deaac2503f642ca15
nechepurenko <denis.nechepurenko@gmail.com>
2016-01-14 14:11:40 +0200

    Можно задать параметр 1 или 2: 1 - играть только нечётные "точки", 2 - только чётные

--- Beeper.php
+++ Beeper.php
@@ -4,6 +4,10 @@ class Beeper
 {
     public function beep($freq, $length, $delay = 0)
     {
+        if ($freq == 0) {
+            usleep(($length + $delay) * 1000);
+            return;
+        }
         $cmd = 'beep -f ' . intval(round($freq)) . ' -l ' . intval(round($length));
         if ($delay) {
             $cmd .= ' -D ' . intval(round($delay));
@@ -11,4 +15,3 @@ class Beeper
         passthru($cmd);
     }
 }
-
--- index.php
+++ index.php
@@ -4,6 +4,8 @@ set_time_limit(120);
 
 require 'Beeper.php';
 
+$arg1 = isset($argv[1]) ? intval($argv[1]) : 0;
+
 $beeper = new Beeper();
 
 $freq = 2000;
@@ -21,9 +23,21 @@ while (true) {
         break;
     }
 }
-$beeper->beep($freq, 100, 900);
-$beeper->beep($freq, 100, 900);
-$beeper->beep($freq, 100, 900);
-$beeper->beep($freq, 100, 900);
-$beeper->beep($freq, 100, 900);
-$beeper->beep($freq, 100 + $hour * 20);
+
+if ($arg1 == 0) {
+    $freq1 = $freq;
+    $freq2 = $freq;
+} elseif ($arg1 == 1) {
+    $freq1 = $freq;
+    $freq2 = 0;
+} elseif ($arg1 == 2) {
+    $freq1 = 0;
+    $freq2 = $freq;
+}
+
+$beeper->beep($freq1, 100, 900);
+$beeper->beep($freq2, 100, 900);
+$beeper->beep($freq1, 100, 900);
+$beeper->beep($freq2, 100, 900);
+$beeper->beep($freq1, 100, 900);
+$beeper->beep($freq2, 100 + $hour * 20);
